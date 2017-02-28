0463e73d1959a3e5533f045cf6539208375dfc78
sunrise2075 <sunrise2075@163.com>
2017-02-28 09:19:20 +0800

    define data model of geo-location

--- src/js/model/localtion.model.js
+++ src/js/model/localtion.model.js
@@ -1,3 +1,20 @@
 /**
  * Created by sunrise2075 on 2017/2/26.
  */
+/*
+* @constructor
+* @description : constructor function to build geo-location object for my Neighborhood App
+* @param title : title of the location
+* @param lat   : latitude of current location
+* @param lng   : longitude of current location
+* */
+function Location(title, lat, lng){
+    this.title = title;
+    this.lat = lat;
+    this.lng = lng;
+}
+
+Location.prototype.toString = function(){
+    var result = "Location:[title]=" + this.title + ",[latitude]=" + this.lat + ",[longitude]=" + this.lng;
+    return result;
+}
